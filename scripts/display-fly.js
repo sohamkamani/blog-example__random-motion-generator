052d221cacfcaf33da057923f5cf1306a7e0677b
Soham Kamani <soham@Sohams-MacBook-Pro.local>
2017-09-10 10:06:50 +0530

    init ;

---
+++ scripts/display-fly.js
@@ -0,0 +1,34 @@
+const TAU = Math.PI*2
+
+const Fly = function(journeyPoints, canvasId){
+  this.journeyPoints = journeyPoints
+  var canvas = document.getElementById(canvasId)
+  const context = canvas.getContext('2d')
+  this.context = context
+  this.position = 0
+}
+
+Fly.prototype.draw = function(){
+  const {context, journeyPoints, position} = this
+  context.beginPath()
+  context.fillStyle = 'white'
+  context.fillRect(0,0,200,200)
+  const {x, y} = journeyPoints[position]
+  context.arc(x, y, 10, 0, TAU)
+  context.stroke()
+}
+
+Fly.prototype.next = function(){
+  this.position += 1
+}
+
+Fly.prototype.animate = function(){
+  requestAnimationFrame(()=>{
+    if(this.position >= this.journeyPoints.length -1){
+      return
+    }
+    this.next()
+    this.draw()
+    this.animate()
+  })
+}
---
+++ scripts/display-lines.js
@@ -0,0 +1,40 @@
+function displayCanvas(canvasId, data) {
+  var canvas = document.getElementById(canvasId)
+  const context = canvas.getContext('2d')
+
+  const margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 50
+  }
+  const width = canvas.width - margin.left - margin.right
+  const height = canvas.height - margin.top - margin.bottom
+
+  var x = d3
+    .scaleLinear()
+    .range([0, width])
+    .domain([0, data.length])
+
+  var y = d3
+    .scaleLinear()
+    .range([height, 0])
+    .domain(d3.extent(data, d => d * 1.3))
+
+  var line = d3
+    .line()
+    .x((_, i) => {
+      return x(i)
+    })
+    .y(d => y(d))
+    .curve(d3.curveCatmullRom)
+    .context(context)
+
+  context.translate(margin.left, margin.top)
+
+  context.beginPath()
+  line(data)
+  context.lineWidth = 1.5
+  context.strokeStyle = 'steelblue'
+  context.stroke()
+}
