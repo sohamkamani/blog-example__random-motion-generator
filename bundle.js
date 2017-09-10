70afbf475d18e04d75f8eac739336348b4e0fa7d
Soham Kamani <soham@Sohams-MacBook-Pro.local>
2017-09-10 13:29:12 +0530

    Animation!!

--- bundle.js
+++ bundle.js
@@ -5554,29 +5554,18 @@ Fly.prototype.animate = function(){
 
 const randomLinesX = genRandomLinesDegree({
   degree: 4,
-  nPoints: 2000,
+  nPoints: 500,
   max: 5,
   min: -5
 });
 const randomLinesY = genRandomLinesDegree({
   degree: 4,
-  nPoints: 2000,
+  nPoints: 500,
   max: 5,
   min: -5
 });
 
-const cl0x = new CanvasLineChart('d0x', randomLinesX[0]);
-const cl0y = new CanvasLineChart('d0y', randomLinesY[0]);
-// const cl1 = new CanvasLineChart('d1', randomLinesX[1])
-// const cl2 = new CanvasLineChart('d2', randomLinesX[2])
-// const cl3 = new CanvasLineChart('d3', randomLinesX[3])
-
-// cl0x.drawChart()
-// cl1.drawChart()
-// cl2.drawChart()
-// cl3.drawChart()
-
-const startFly = (dataX, dataY) => {
+const startFly = (dataX, dataY, canvasId) => {
   var flyX = linear$2()
     .range([0, 200])
     .domain(extent(dataX, d => d * 1.5));
@@ -5589,11 +5578,25 @@ const startFly = (dataX, dataY) => {
     x: flyX(d),
     y: flyY(dataY[i])
   }));
-  const fly = new Fly(flyPath, 'd0f');
+  const fly = new Fly(flyPath, canvasId);
   return fly
 };
 
-const fly = startFly(randomLinesX[0], randomLinesY[0]);
+const fly0 = startFly(randomLinesX[0], randomLinesY[0], 'd0f');
+const cl0x = new CanvasLineChart('d0x', randomLinesX[0]);
+const cl0y = new CanvasLineChart('d0y', randomLinesY[0]);
+
+const fly1 = startFly(randomLinesX[1], randomLinesY[1], 'd1f');
+const cl1x = new CanvasLineChart('d1x', randomLinesX[1]);
+const cl1y = new CanvasLineChart('d1y', randomLinesY[1]);
+
+const fly2 = startFly(randomLinesX[2], randomLinesY[2], 'd2f');
+const cl2x = new CanvasLineChart('d2x', randomLinesX[2]);
+const cl2y = new CanvasLineChart('d2y', randomLinesY[2]);
+
+const fly3 = startFly(randomLinesX[3], randomLinesY[3], 'd3f');
+const cl3x = new CanvasLineChart('d3x', randomLinesX[3]);
+const cl3y = new CanvasLineChart('d3y', randomLinesY[3]);
 
 const animate = (fly, chartX, chartY) =>
   requestAnimationFrame(() => {
@@ -5605,7 +5608,18 @@ const animate = (fly, chartX, chartY) =>
     }
   });
 
-animate(fly, cl0x, cl0y);
+animate(fly0, cl0x, cl0y);
+animate(fly1, cl1x, cl1y);
+animate(fly2, cl2x, cl2y);
+animate(fly3, cl3x, cl3y);
+
+const regen = document.getElementById('d0regen');
+regen.addEventListener('click', () => {
+  fly0.position = 0;
+  cl0x.position = 0;
+  cl0y.position = 0;
+  animate(fly0, cl0x, cl0y);
+});
 
 }());
 //# sourceMappingURL=bundle.js.map
